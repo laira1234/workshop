a3b0b969d85260d127fbbca01159045cf79ead4e
laira1234 <Lairabianca9@gmail.com>
2024-11-25 10:02:44 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'cozinhar',
+    'como fazer um miojo?',
+    'coloque a água pra ferver, depois de ferver água coloque o miojo, depois de 5 min retire e tempere com o sachê'
+)
+
+criaCartao(
+    'cozinhar',
+    'como fazer um ovo cozido?',
+    'coloque o ovo em uma panela com água e deixe ferver, depois de alguns min retire, tire a casca e proto'
+)
+
+criaCartao(
+    'cozinhar',
+    'como fazer batata na frita air fryer?',
+    'pegue a batata congela, coloque na air fryer, coloque o min desejado no temporizador e espere, quando o ciclo acabar terminou'
+)
+
+criaCartao(
+    'cozinhar',
+    'como temperar uma salada de alface?',
+    'corte o alface, coloque em um recipiente, adicione sal, coloque azeite ou óleo, acrecente limão ou vinagre pta finalizar coloque pimenta do reino a gosto'
+)
